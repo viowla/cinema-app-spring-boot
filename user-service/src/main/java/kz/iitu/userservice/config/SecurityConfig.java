6d631e7fe8d3bed9ddceb9454257c7daec345ca0
viowla <55015538+viowla@users.noreply.github.com>
2020-09-26 11:47:25 +0600

    1.add config package and JWT token, spring and security configurations 2.add jwt dependency in pom.xml

---
+++ user-service/src/main/java/kz/iitu/userservice/config/SecurityConfig.java
@@ -0,0 +1,69 @@
+package kz.iitu.userservice.config;
+
+import kz.iitu.userservice.services.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/users/create", "/users").permitAll()
+                .antMatchers("/", "/registration").permitAll()
+                //.antMatchers("/main").permitAll()
+                .antMatchers("/users/**").hasAuthority("ADMIN")
+                .antMatchers("/role/**").hasAuthority("ADMIN")
+                .antMatchers("/dessert/create").hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                //.autnenticated
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/")
+                .and()
+                .logout().permitAll()
+                .and()
+                // What's the authenticationManager()?
+                // An object provided by WebSecurityConfigurerAdapter, used to authenticate the user passing user's credentials
+                // The filter needs this auth manager to authenticate the user.
+
+                .addFilter(new JwtTokenGen(authenticationManager()))
+
+                // Add a filter to validate the tokens with every request
+                .addFilterAfter(new JwtTokenAuth(), UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs/**");
+        web.ignoring().antMatchers("/swagger.json");
+        web.ignoring().antMatchers("/swagger-ui.html");
+        web.ignoring().antMatchers("/swagger-resources/**");
+        web.ignoring().antMatchers("/webjars/**");
+    }
+}
