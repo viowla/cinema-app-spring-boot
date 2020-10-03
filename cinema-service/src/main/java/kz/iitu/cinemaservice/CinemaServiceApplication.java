9d162f62800f43843d4be3e5e7e3d19a80514039
viowla <55015538+viowla@users.noreply.github.com>
2020-10-03 16:21:50 +0600

    edit ServiceApplication classes' annotations

--- cinema-service/src/main/java/kz/iitu/cinemaservice/CinemaServiceApplication.java
+++ cinema-service/src/main/java/kz/iitu/cinemaservice/CinemaServiceApplication.java
@@ -2,12 +2,18 @@ package kz.iitu.cinemaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class CinemaServiceApplication {
 
     @Bean
